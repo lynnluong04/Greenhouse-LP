37f413cfc51404bc6fa488efde423bd3a9fcde9e
Lynn Luong <lynn.h.luong@gmail.com>
2022-06-16 16:15:17 -0400

    Phase 3 done

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,21 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useState } from "react";
+
+export const ClimateContext = createContext();
+
+export default function ClimateProvider() {
+    const [temperature, setTemperature] = useState(50)
+
+    return (
+        <ClimateContext.Provider
+            value={{
+                temperature,
+                setTemperature
+            }}
+        >
+            {children}
+        </ClimateContext.Provider>
+    );
+}
