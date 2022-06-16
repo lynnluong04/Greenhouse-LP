06c39b87b09d0f630679f524b233c47723166be6
Lynn Luong <lynn.h.luong@gmail.com>
2022-06-16 17:06:37 -0400

    Phase 6 done

--- src/components/Greenhouse/ClimateStats.js
+++ src/components/Greenhouse/ClimateStats.js
@@ -2,14 +2,15 @@ import './ClimateStats.css';
 import {useTemperature} from '../../context/ClimateContext';
 
 function ClimateStats() {
-  const {temperature, setTemperature} = useTemperature()
+  const {temperature, humidity} = useTemperature()
+
   return (
     <div className="climate-stats">
       <div className="temperature">
         Temperature {temperature}°F
       </div>
       <div className="humidity">
-        Humidity {"y"}%
+        Humidity {humidity}%
       </div>
     </div>
   )
--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -4,16 +4,21 @@
 import { createContext, useContext, useState } from "react";
 
 export const ClimateContext = createContext();
+
 export const useTemperature = () => useContext(ClimateContext)
+
 
 export default function ClimateProvider(props) {
     const [temperature, setTemperature] = useState(50)
+    const [humidity, setHumidity] = useState(40)
 
     return (
         <ClimateContext.Provider
             value={{
                 temperature,
-                setTemperature
+                setTemperature,
+                humidity,
+                setHumidity
             }}
         >
             {props.children}
